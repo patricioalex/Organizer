4881023bfc304e9b7ea6c20fce037a6e1b481605
Patrício Alex <46056058+patricioalex96@users.noreply.github.com>
2020-10-03 18:30:50 -0300

    Atribuindo mais interface
    Alteração de métodos

--- organizer.php
+++ organizer.php
@@ -10,31 +10,47 @@ require_once('controller/Control.php');
  * 2° Se o argumento é um diretório
  * 3° Se o diretório pode ser aberto
  * 4° Looping para cada tipo de arquivo que for achado
+ * 5° Ignorando os pontos do sistema 
+ *    $arrayFile: Atributos recebendo uma array do nome do arquivo separado para cada ponto achado
+ * 6° Se o atributo $arrayFile tem dois index na array para sabermos depois se é uma arquivo sem extensão ou um diretório
+ *    $extension: Pegando a extensão que sempre será o ultimo do index da array
+ * 
  */
 
-$dir = $argv[1];
+$controller = new Control();
 
-// if(isset($dir)){
-//     if(is_dir($dir)){
-//         if(opendir($dir)){
-//             while($file = readdir()){
-//                 $arrayFile = explode('.', $file);
-//                 $extension = end($arrayFiles);
-//                 if(control::folderExists($dir, $extension)){
-//                     foreach ($arrayFiles as $file ) {
-//                         # code...
-//                     }                  
-//                 }
-//             }
-//         }else{
-//             echo "\n\n". Control::message($mensagem = 3) ."\n\n";
-//         }
-//     }else{
-//         echo "\n\n". Control::message($mensagem = 2) ."\n\n";
-//     }    
-// }else{
-//     echo "\n\n". Control::message($mensagem = 1) ."\n\n";
-// }
+
+if(count($argv) >= 2){
+  if(is_dir($dir = $argv[1])){
+    if($controller->list($dir)){
+      while($file = readdir()){
+        if($file != '.' && $file != ".."){
+          $arrayFile = explode('.', $file);
+          if(count($arrayFile) >= 2){
+            $extension = end($arrayFile);              
+            if($controller->folderExists($dir, $extension)){
+              // while($fileType = readdir()){
+              //   echo $fileType ."\n"   ;
+                rename($dir.$file, $dir.$controller->nameFolder().'/'.$file);
+              // }
+            //     // foreach ($arrayFiles as $file ) {
+            //     //     # code...
+            //     // }    
+                echo $extension ."\n"   ;
+            }                     
+          }
+        }
+      }
+    }else{
+        echo "\n\n". $controller->message($mensagem = 3) ."\n\n";
+    }
+    closedir();
+  }else{
+      echo "\n\n". $controller->message($mensagem = 2) ."\n\n";
+  }    
+}else{
+    echo "\n\n". $controller->message($mensagem = 1) ."\n\n";
+}
 
 
 
@@ -55,10 +71,10 @@ $dir = $argv[1];
 //     echo "Tem mac";
 // }
 // $all = array();
-opendir($dir);
-while($file = readdir()){
-    $all[] = $file;
-  }
-  closedir($dir);
-  $teste[] = $all;
-  print_r($teste);
+// opendir($dir);
+// while($file = readdir()){
+//     $all[] = $file;
+//   }
+//   closedir($dir);
+//   // $teste[] = $all;
+//   print_r($all);
